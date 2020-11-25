b38ad0008a4e7e3225e956a15947d96713b2d652
Леонтьев Михаи <tennantonelove@yandex.ru>
2020-11-25 17:20:25 +0400

    Все функции готовы.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QMainWindow, QApplication, QMessageBox
+from PyQt5.QtWidgets import QMainWindow, QApplication, QMessageBox, QTableWidgetItem
 from PyQt5 import uic
 
 import sys
@@ -9,6 +9,34 @@ class Window(QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi('main.ui', self)
+        self.con = sqlite3.connect('coffee.sqlite')
+        self.tw.itemDoubleClicked.connect(self.open_inf)
+
+        self.fill_table()
+
+    def fill_table(self):
+        self.tw.setColumnCount(7)
+        self.tw.setHorizontalHeaderLabels(
+            ['ID', 'Название', 'Степень обжарки', 'Тип',
+             'Описание', 'Цена', 'Объём упаковки'])
+
+        cur = self.con.cursor()
+        for i in cur.execute('''SELECT * FROM Drinks
+        INNER JOIN Roasts
+        ON Drinks.roast=Roasts.id''').fetchall():
+            i = i[:2] + (i[-1],) + i[3:-2]
+            self.tw.setRowCount(self.tw.rowCount() + 1)
+            for j in enumerate(i):
+                print(j)
+                if j[0] == 3:
+                    j = (j[0], 'Молотый' if j[1] else 'В зёрнах')
+                self.tw.setItem(self.tw.rowCount() - 1, j[0], QTableWidgetItem(str(j[1])))
+
+    def open_inf(self, item):
+        mes = QMessageBox.information(self,
+                                      'Просмотр элемента',
+                                      item.text(),
+                                      QMessageBox.Ok, QMessageBox.Ok)
 
 
 if __name__ == '__main__':
